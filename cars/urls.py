b2e2e7a73c34ae50274761083838fb71a25b606a
amin <kheyrriamin@yahoo.com>
2021-06-06 08:31:59 +0430

    Search Forms Search Functionality & Working with Admin Styles

--- cars/urls.py
+++ cars/urls.py
@@ -6,4 +6,5 @@ app_name = 'cars'
 urlpatterns = [
     path('', views.cars, name='cars'),
     path('<int:id>/car_detail', views.car_detail, name='car_detail'),
+    path('search/', views.search, name='search')
 ]
--- pages/views.py
+++ pages/views.py
@@ -7,10 +7,18 @@ def home(request):
     teams = Teams.objects.all()
     featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
     all_cars = Car.objects.order_by('created_date')
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style = Car.objects.values_list('body_style', flat=True).distinct()
     data = {
         'teams': teams,
         'featured_cars': featured_cars,
         'all_cars': all_cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style': body_style,
     }
     return render(request, 'pages/home.html', data)
 
